a20e822f0dd4af6ae7eeb788f78373ee25085742
Šime Vidas <sime.vidas@gmail.com>
2017-04-01 21:16:26 +0200

    Add test case for preact-markup

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,31 @@
 import { h, Component } from 'preact';
 import style from './style';
 
+import axios from 'axios';
+import marked from 'marked';
+import Markup from 'preact-markup';
+
 export default class Home extends Component {
-	render() {
+	constructor() {
+		super();
+
+		this.state = {
+			markup: '<p>Loading…</p>'
+		};
+	}
+
+	componentDidMount() {
+		axios.get('/assets/basic-sample.md').then(
+			({ data }) => this.setState({ 
+				markup: marked(data) 
+			})
+		);
+	}
+
+	render(props, { markup }) {
 		return (
 			<div class={style.home}>
-				<h1>Home</h1>
-				<p>This is the Home component.</p>
+				<Markup markup={markup} type="html"/>
 			</div>
 		);
 	}
